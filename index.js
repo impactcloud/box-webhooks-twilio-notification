fb1ea51842046cd0947879405d409b5aecba4452
Cary Cheng <ccheng@box.com>
2017-10-30 17:58:22 -0700

    init, still needs setup

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-var express = require('express')
-var app = express()
+var express = require('express'),
+    app = express(),
+    twilio = require(twilio),
+    accountSid = '',
+    authToken = '',
+    BoxSDK = require('box-node-sdk');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -7,6 +11,32 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
+
+app.get('/create_webhook', function(request, response){
+  var sdk = new BoxSDK({
+    clientID: '',
+    clientSecret: ''
+  });
+
+  var client = sdk.getBasicClient('');
+
+  client.webhooks.create('', client.itemTypes.FILE, '', [client.webhooks.triggerTypes.FILE.UPLOADED], function(err, data){
+    if(err){
+      console.log('Failed to create Box Webhook successfully');
+    }
+  });
+});
+
+app.get('/send_sms', function(request, response) {
+  var client = new twilio(accountSid, authToken);
+  
+  client.messages.create({
+      body: 'Volunteer Form Update',
+      to: '6504171570',  // Text this number
+      from: '+12345678901' // From a valid Twilio number
+  })
+  .then((message) => console.log(message.sid));
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
